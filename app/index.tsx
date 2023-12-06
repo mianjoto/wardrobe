2bf0f272d8cfb3f938db86067cb01a4b488e24df
mianjoto <mianjoto@gmail.com>
2023-12-05 21:37:59 -0500

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface IndexPageProps {}
+
+const IndexPage = ({}: IndexPageProps) => {
+  return (
+    <View>
+      <Text>Index page</Text>
+    </View>
+  );
+};
+
+export default IndexPage;
